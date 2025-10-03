c0a0f3755cc540cca7adcad11371d1e0b2f7abe9
JayCrlt <jeremy.carlot@hotmail.fr>
2025-10-03 17:02:42 +0200

    add change over time

--- Scripts/04_temporal_change.R
+++ Scripts/04_temporal_change.R
@@ -3,7 +3,7 @@ rm(list = ls()) ; options(warn = -1)
 library("rfishbase") ; library("phylosem") ; library("tidyverse") ; library('readxl') ; library("scales")
 library("fishtree") ; library("geiger") ; library("ape") ; library("Rphylopars") ; library("brms") ; library("sf")
 library("ggridges") ; library("patchwork") ; library("fishflux") ; library("leaflet") ; library("leaflet.extras")
-library("rnaturalearth") ; library("rnaturalearthdata")
+library("rnaturalearth") ; library("rnaturalearthdata") ; library("broom")
 
 ## Download data
 sp_code_list       <- read.delim("Data/MEDITS_spp.codes.csv", sep = ";")
@@ -16,8 +16,10 @@ load("Outputs/dat_proc/Medit_Temp.RData")
 load("Outputs/dat_proc/medits_sf_percentile.Rdata")
 load("Outputs/dat_proc/Medit_FunCatch_without_NA.RData")
 
-## Color palette
-c("#a50026","#d73027","#f46d43","#fdae61","#fee090","#ffffff","#e0f3f8","#abd9e9","#74add1","#4575b4","#313695")
+## Color palette and map
+land       <- ne_countries(scale = "medium", returnclass = "sf")
+my_palette <- c("#a50026","#d73027","#f46d43","#fdae61","#fee090","#ffffff",
+               "#e0f3f8","#abd9e9","#74add1","#4575b4","#313695")
 
 ## Functions
 source("Scripts/00_functions_script.R")
@@ -43,32 +45,36 @@ medits_with_hexID <- st_join(medits_sf_percentile, st_transform(Hexagonal_grid, 
 ## Keep only HEX cells with at least 10 years of data
 medits_with_hexID_over_10years <- medits_with_hexID |> 
   filter(HEX_ID %in% (medits_with_hexID |> group_by(HEX_ID) |> summarise(n_years = n_distinct(YEAR)) |> 
-                        filter(n_years >= 10) |> st_drop_geometry() |> ungroup() |> select(HEX_ID) |> pull(HEX_ID))) |> 
-  group_by(HEX_ID) |> 
-  # Define outliers for Gc
-  mutate(Q1 = quantile(community_Gc, 0.25, na.rm = T), Q3 = quantile(community_Gc, 0.75, na.rm = T), IQR = Q3 - Q1,
-  outlier_Gc = ifelse(community_Gc < (Q1 - 1.5 * IQR) | community_Gc > (Q3 + 1.5 * IQR), "outlier", NA)) |> 
-  select(-Q1, -Q3, -IQR) |> ungroup() |> 
-  # Define outliers for Fn
-  mutate(Q1 = quantile(community_Fn, 0.25, na.rm = T), Q3 = quantile(community_Fn, 0.75, na.rm = T), IQR = Q3 - Q1,
-  outlier_Fn = ifelse(community_Fn < (Q1 - 1.5 * IQR) | community_Fn > (Q3 + 1.5 * IQR), "outlier", NA)) |> 
-  select(-Q1, -Q3, -IQR) |> ungroup() |> 
-  # Define outliers for Fp
-  mutate(Q1 = quantile(community_Fp, 0.25, na.rm = T), Q3 = quantile(community_Fp, 0.75, na.rm = T), IQR = Q3 - Q1,
-  outlier_Fp = ifelse(community_Fp < (Q1 - 1.5 * IQR) | community_Fp > (Q3 + 1.5 * IQR), "outlier", NA)) |> 
-  select(-Q1, -Q3, -IQR) |> ungroup() |>  
-  # Define outliers for Plank
-  mutate(Q1 = quantile(Ic_plank, 0.25, na.rm = T), Q3 = quantile(Ic_plank, 0.75, na.rm = T), IQR = Q3 - Q1,
-  outlier_plank = ifelse(Ic_plank < (Q1 - 1.5 * IQR) | Ic_plank > (Q3 + 1.5 * IQR), "outlier", NA)) |> 
-  select(-Q1, -Q3, -IQR) |> ungroup() |> 
-  # Define outliers for Benth
-  mutate(Q1 = quantile(Ic_benthivorous, 0.25, na.rm = T), Q3 = quantile(Ic_benthivorous, 0.75, na.rm = T), IQR = Q3 - Q1,
-  outlier_benthivorous = ifelse(Ic_benth < (Q1 - 1.5 * IQR) | Ic_benth > (Q3 + 1.5 * IQR), "outlier", NA)) |> 
-  select(-Q1, -Q3, -IQR) |> ungroup()
+                        filter(n_years >= 10) |> st_drop_geometry() |> ungroup() |> select(HEX_ID) |> 
+                        pull(HEX_ID))) |> group_by(HEX_ID) 
 
-hist(log(medits_sf_percentile$Ic_benthivorous))
+## Analyze slopes over year
+Slopes <- st_as_sf(medits_with_hexID_over_10years %>%
+  pivot_longer(cols = c(community_Gc, community_Fn, community_Fp, Ic_plank, Ic_benthivorous),
+    names_to = "metric", values_to = "value") %>% group_by(HEX_ID, metric) %>% do(tidy(lm(value ~ YEAR, data = .))) %>%
+  filter(term == "YEAR") %>% select(HEX_ID, metric, slope = estimate) %>%
+  pivot_wider(names_from = metric, values_from = slope, names_glue = "{metric}_slope") %>%
+  left_join(medits_with_hexID_over_10years %>% st_drop_geometry() %>% group_by(HEX_ID) %>%
+      summarise(centroid_lon = mean((left + right) / 2, na.rm = TRUE),
+                centroid_lat = mean((top + bottom) / 2, na.rm = TRUE)) %>%
+      st_as_sf(coords = c("centroid_lon", "centroid_lat"), crs = st_crs(medits_with_hexID_over_10years))))
 
-## Analyze Gc slopes over year
-ggplot(data = medits_with_hexID_over_10years |> dplyr::filter(HEX_ID %in% c(1057, 1177)), 
-       aes(y = community_Gc, x = YEAR, group = HEX_ID)) + geom_line() +
-  geom_point(aes(fill = outlier_Gc), shape = 21, size = 3)
+
+ggplot(Slopes) + 
+  geom_sf(aes(fill = community_Gc_slope), color = "black", shape = 21) +
+  scale_fill_gradientn(colors = my_palette, 
+                       name = "Gc slope",
+                       na.value = "grey90") +
+  geom_sf(data = land, fill = "lightgray", color = "black") +
+  theme_minimal() + 
+  coord_sf(xlim = c(-5, 35), ylim = c(34, 46)) + 
+  theme(
+    panel.border    = element_rect(color = "black", fill = NA, size = 1),
+    plot.title      = element_text(size = 20, hjust = 0),
+    axis.title      = element_text(size = 18),
+    axis.text       = element_text(size = 16),
+    legend.title    = element_text(size = 14),
+    legend.text     = element_text(size = 12),
+    legend.position = "bottom"
+  )
+hist(Slopes$community_Fp_slope)
